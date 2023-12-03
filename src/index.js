23396e1922ef67560e04664445cc76ed80ab2035
kleidsoncaldas <kleidson333@gmail.com>
2023-12-03 20:55:44 -0300

    att desafio nivel heroi

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
-// Desafio Classificador de nível de Herói
+let nomeDoHeroi = "traker";
+let xp = 10001;
 
-let nomeDoHeroi = "traker" 
-let xp = 5000
+let nivel;
 
-const mensagem = " Esta no nivel de "
-
-if (xp < 1000){
-    nivel = "ferro"
-} else if ( xp > 1.001 && xp < 2000){
-     nivel = "bronze"
-} else if ( xp > 2.001 && xp < 5000){
-    nivel = "prata"
-} else if (xp > 6.001 && xp < 7000){
-    nivel = "ouro"
-} else if ( xp > 7.001 && xp < 8000){
-    nivel = "platina"
-} else if ( xp > 8.001 && xp < 9.000){
-    nivel = "Ascendente"
-} else if (xp > 9.001 && xp < 10.000){
-    nivel = "Imortal"
-} else if (xp >= 10.001){
-    nivel = "radiante"
+if (xp < 1000) {
+    nivel = "ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "radiante";
 }
 
-console.log ("O Héroi de nomne " + nomeDoHeroi + " está no nivél de " + nivel)
-  
+console.log("O Héroi de nome " + nomeDoHeroi + " está no nível de " + nivel);
     
     
 
